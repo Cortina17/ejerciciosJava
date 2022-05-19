17042e3f33c669adc3d77e9d67827d03ff4d483e
diego <diego.cortinagarcia@gmail.com>
2022-05-19 16:55:03 +0200

    fourth exercise

--- src/ejercicios/Ejercicio2.java
+++ src/ejercicios/Ejercicio2.java
@@ -5,6 +5,7 @@ public class Ejercicio2 {
 	public static void main(String[] args) {
 		media();
 		evenOdd();
+		category();
 	}
 
 	static void media() {
@@ -18,5 +19,20 @@ public class Ejercicio2 {
 	static void evenOdd() {
 		int number = 5434532;
 		System.out.println((number % 2 == 0) ? "Even number" : "Odd number");
+	}
+
+	static void category() {
+
+		String cat = "b";
+
+		if (cat == "a") {
+			System.out.println("Hijo");
+		} else if (cat == "b") {
+			System.out.println("Padre");
+		} else if (cat == "c") {
+			System.out.println("Abuelo");
+		} else {
+			System.out.println("Error");
+		}
 	}
 }
