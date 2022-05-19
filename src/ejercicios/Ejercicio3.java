5e90577a156cdc298a8c8ec205ca95b41ffe97e2
diego <diego.cortinagarcia@gmail.com>
2022-05-19 17:09:46 +0200

    sixth exercise

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -3,8 +3,15 @@ package ejercicios;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		table();
 
 	}
 
+	static void table() {
+		int i = 2;
+
+		for (int j = 0; j < 11; j++) {
+			System.out.println((i * j));
+		}
+	}
 }
