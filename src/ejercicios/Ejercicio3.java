c7a90a14c418535d7d53483499d71a55e969eead
diego <diego.cortinagarcia@gmail.com>
2022-05-19 17:20:14 +0200

    exercise tryout 3.2

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -4,6 +4,7 @@ public class Ejercicio3 {
 
 	public static void main(String[] args) {
 		table();
+		netSalary();
 
 	}
 
@@ -13,5 +14,19 @@ public class Ejercicio3 {
 		for (int j = 0; j < 11; j++) {
 			System.out.println((i * j));
 		}
+	}
+
+	static void netSalary() {
+
+		byte ant = 3;
+		String cat = "A";
+		int salary;
+		int totalSalary;
+
+		if (ant > 1 && ant <= 5) {
+			totalSalary = salary + (salary * (5 / 100));
+		}
+
+		System.out.println("Total salary is the result of ");
 	}
 }
