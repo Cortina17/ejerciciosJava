99e1a07dac1d24b729fbab5196462ddaec57564a
diego <diego.cortinagarcia@gmail.com>
2022-05-20 11:54:17 +0200

    examples

--- src/ejercicios/Empleado.java
+++ src/ejercicios/Empleado.java
@@ -1,12 +1,9 @@
 package ejercicios;
 
-import java.util.Date;
-
 public class Empleado {
 
 	protected String name;
 	protected double salary;
-	protected Date birthDate;
 
 	public String getDetails() {
 		return "Name: " + name + "\n" + "Salary: " + salary;
--- src/ejercicios/TestHerencia.java
+++ src/ejercicios/TestHerencia.java
@@ -1,7 +1,5 @@
 package ejercicios;
 
-import java.util.Date;
-
 public class TestHerencia {
 
 	public static void main(String[] args) {
