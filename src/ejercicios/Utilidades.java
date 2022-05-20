c440c754b1cbd2826cb069a09a789db1776d89b9
diego <diego.cortinagarcia@gmail.com>
2022-05-20 09:56:06 +0200

    added more to refactor

--- src/ejercicios/Utilidades.java
+++ src/ejercicios/Utilidades.java
@@ -1,15 +1,33 @@
 package ejercicios;
 
 import java.util.Date;
-import java.time.LocalDateTime; 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-
 
 public class Utilidades {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static int day;
+	private static int month;
+	private static int year;
 
+	public static void main(String[] args) {
+		daysPerMonth(day);
+		leapYear(day);
+		greet();
+	
 	}
 
+	public static int daysPerMonth(int month) {
+		return 4;
+	}
+
+	public static boolean leapYear(int year) {
+		return true;
+	}
+
+	static void greet() {
+		LocalTime timeStamp = LocalTime.now();
+		System.out.println(timeStamp);
+	}
 }
