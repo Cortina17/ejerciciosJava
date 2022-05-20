49e0bf0738b001003ec036cbde976c0a22c096c0
diego <diego.cortinagarcia@gmail.com>
2022-05-20 09:20:30 +0200

    refactored exercises

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -9,24 +9,40 @@ public class Ejercicio3 {
 	}
 
 	static void table() {
-		int i = 2;
+		int i = 7;
 
 		for (int j = 0; j < 11; j++) {
-			System.out.println((i * j));
+			System.out.println((i + " X " + j + " = " + i * j));
 		}
 	}
 
 	static void netSalary() {
 
 		byte ant = 3;
-		String cat = "A";
-		int salary;
-		int totalSalary;
+		char cat = 'C';
+		double salary = 1000;
+		double totalSalary = 0;
 
 		if (ant > 1 && ant <= 5) {
-			totalSalary = salary + (salary * (5 / 100));
+			totalSalary = salary * 1.05;
+		}
+		if (ant > 6 && ant <= 10) {
+			totalSalary = salary * 1.10;
+		}
+		if (ant > 10) {
+			totalSalary = salary * 1.30;
 		}
 
-		System.out.println("Total salary is the result of ");
+		switch (cat) {
+		case 'A':
+			totalSalary = totalSalary + 1000;
+		case 'B':
+			totalSalary = totalSalary + 2000;
+		case 'C':
+			totalSalary = totalSalary + 3000;
+		}
+
+		System.out.println("Total salary is: " + totalSalary + " euros");
 	}
+
 }
